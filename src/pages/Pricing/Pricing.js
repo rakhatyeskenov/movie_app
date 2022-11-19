1540459f042e06d8fc2ebc58706bfbf41663f02f
Rakhat <eskenovrahat@gmail.com>
2022-11-20 03:59:24 +0600

    Color changes

--- src/pages/Pricing/Pricing.js
+++ src/pages/Pricing/Pricing.js
@@ -10,10 +10,10 @@ const Pricing = () => {
                 <ul className="price">
                     <li className="header__ header__basic   ">Free</li>
                     <li className="grey">Free</li>
-                    <li>2GB Storage</li>
-                    <li>2 Emails</li>
-                    <li>2 Domains</li>
-                    <li>500MB Bandwidth</li>
+                    <li className="li__">2GB Storage</li>
+                    <li className="li__">2 Emails</li>
+                    <li className="li__">2 Domains</li>
+                    <li className="li__">500MB Bandwidth</li>
                     <li className="grey"><a href="#" className="button">Sign Up</a></li>
                 </ul>
             </div>
@@ -21,10 +21,10 @@ const Pricing = () => {
                 <ul className="price">
                     <li className="header__ header__basic">Basic</li>
                     <li className="grey">$ 9.99 / year</li>
-                    <li>10GB Storage</li>
-                    <li>10 Emails</li>
-                    <li>10 Domains</li>
-                    <li>1GB Bandwidth</li>
+                    <li className="li__">10GB Storage</li>
+                    <li className="li__">10 Emails</li>
+                    <li className="li__">10 Domains</li>
+                    <li className="li__">1GB Bandwidth</li>
                     <li className="grey"><a href="#" className="button">Sign Up</a></li>
                 </ul>
             </div>
@@ -32,10 +32,10 @@ const Pricing = () => {
                 <ul className="price">
                     <li className="header__ header__pro">Pro</li>
                     <li className="grey">$ 24.99 / year</li>
-                    <li>25GB Storage</li>
-                    <li>25 Emails</li>
-                    <li>25 Domains</li>
-                    <li>2GB Bandwidth</li>
+                    <li className="li__">25GB Storage</li>
+                    <li className="li__">25 Emails</li>
+                    <li className="li__">25 Domains</li>
+                    <li className="li__">2GB Bandwidth</li>
                     <li className="grey"><a href="#" className="button">Sign Up</a></li>
                 </ul>
             </div>
